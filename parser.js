d5b2a326c4bf484c084740b7d29e82597b6b2af0
Linus Tucker <172833324+MrStarfleetCommand@users.noreply.github.com>
2025-12-17 15:39:10 -0500

    Improve `txtToHtml` parsing
    Refactor heading and paragraph parsing to match start of line and improve whitespace handling.

--- parser.js
+++ parser.js
@@ -1,11 +1,12 @@
 function txtToHtml(txt){
 	let html = txt;
-	html = html.replace(/^\s*(======) *(.+?) *\1 *$/gm, '<h6 class="heading">$2</h6>');
-	html = html.replace(/^\s*(=====) *(.+?) *\1 *$/gm, '<h5 class="heading">$2</h5>');
-	html = html.replace(/^\s*(====) *(.+?) *\1 *$/gm, '<h4 class="heading">$2</h4>');
-	html = html.replace(/^\s*(===) *(.+?) *\1 *$/gm, '<h3 class="heading">$2</h3>');
-	html = html.replace(/^\s*(==) *(.+?) *\1 *$/gm, '<h2 class="heading">$2</h2>');
-	html = html.replace(/^\s*([^<\s].*) *$/gm, '<p>$1</p>');
+	html = html.replace(/\s+$/, '');
+	html = html.replace(/^(======) *(.+?) *\1 *$/gm, '<h6 class="heading">$2</h6>');
+	html = html.replace(/^(=====) *(.+?) *\1 *$/gm, '<h5 class="heading">$2</h5>');
+	html = html.replace(/^(====) *(.+?) *\1 *$/gm, '<h4 class="heading">$2</h4>');
+	html = html.replace(/^(===) *(.+?) *\1 *$/gm, '<h3 class="heading">$2</h3>');
+	html = html.replace(/^(==) *(.+?) *\1 *$/gm, '<h2 class="heading">$2</h2>');
+	html = html.replace(/^ *([^<\s].*?) *$/gm, '<p>$1</p>');
 	html = html.replace(/''' *([^'].*?) *'''/g, '<span class="bold">$1</span>');
 	html = html.replace(/'' *([^'].*?) *''/g, '<span class="italic">$1</span>');
 	html = html.replace(/\[ *(\S+?) +(.+?) *\]/g, '<a href="$1" class="external-link">$2</a>');
