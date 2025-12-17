9e7dc38f34511807617d2d69752f7a6116870d5f
Linus Tucker <172833324+MrStarfleetCommand@users.noreply.github.com>
2025-12-17 16:53:16 -0500

    Add page setup script

---
+++ scripts.js
@@ -0,0 +1,14 @@
+import {txtToHtml, htmlToTxt} from 'parser.js';
+(async () => {
+	const currentPage = location.pathname.replace(/^\/parser\/(.*)\.html$/, '$1');
+	const pageName = currentPage ? currentPage : 'index';
+	const pageSource = await fetch(`${pageName}.txt`);
+	const txt = await pageSource.text();
+	const pageHeading = document.createElement('h1');
+	const parserOutput = document.createElement('div');
+	pageHeading.innerText = document.title;
+	parserOutput.id = 'parser-output';
+	parserOutput.innerHTML = txtToHtml(txt)
+	document.body.appendChild(pageHeading);
+	document.body.appendChild(parserOutput);
+})();
